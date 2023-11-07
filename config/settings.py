db04a3a304df51d51cba40091e7bf08fac03c76c
Smarttizim <126737358+Smarttizim@users.noreply.github.com>
2023-11-07 14:52:54 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
